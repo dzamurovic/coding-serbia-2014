e7c216452aa5386374b741f3f9e9f35007071bac
dzamurovic <ezamur@gmail.com>
2014-09-22 01:36:57 +0200

    Adding XL data size in data generator.

--- codingserbia-data-generator/src/main/java/com/codingserbia/datagenerator/DataSize.java
+++ codingserbia-data-generator/src/main/java/com/codingserbia/datagenerator/DataSize.java
@@ -5,8 +5,9 @@ public enum DataSize {
     S(3);
 
     // S(150000);
-    // M(7500000);
-    // L(15000000);
+    // M(750000);
+    // L(1500000);
+    // XL(2250000);
 
     private DataSize(int size) {
         this.size = size;
