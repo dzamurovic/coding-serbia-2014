792c74d0e903b5a646c044bf2775d4ac8b91feee
dzamurovic <ezamur@gmail.com>
2014-09-24 17:03:19 +0200

    Renaming pig script file.

--- codingserbia-pig/src/test/java/de/codecentric/PigAggregationTest.java
+++ codingserbia-pig/src/test/java/de/codecentric/PigAggregationTest.java
@@ -9,36 +9,40 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PigAggregationTest {
-	
-	private static final String TEST_PATH = "src/test/resources/";
-	
-	private static PigTest test;
-	
-	@BeforeClass
-	public static void setUp() throws IOException, ParseException {
-	    test = new PigTest("src/main/resources/example.pig");
-	    test.override("products", "products = LOAD '" + TEST_PATH + "input/customer_records_map_reduce_input.json' USING JsonLoader('sessionId:int,customerCategoryId:int,products:{(id:int,name:chararray,category:chararray,bought:boolean,price:double)}');");
-	    test.override("categories", "categories = LOAD '" + TEST_PATH + "input/customer_categories.db' AS (categoryId:int,age:chararray,gender:chararray);");
-	}
-	
-	@Test
-	public void testTopFiveProducts() throws IOException, ParseException {
-	    test.assertOutput("resultTopFiveProducts", new File(TEST_PATH + "results/resultTopFiveProducts.txt"));
-	}
 
-	@Test
-	public void testAverageSeenProducts() throws IOException, ParseException {
-	    test.assertOutput("averageCountedProducts", new File(TEST_PATH + "results/averageSeenProducts.txt"));
-	}
-	
-	@Test
-	public void testAverageBoughtProducts() throws IOException, ParseException {
-	    test.assertOutput("resultAverageBoughtProducts", new File(TEST_PATH + "results/averageBoughtProducts.txt"));
-	}
-	
-	@Test
-	public void testAverageSpent() throws IOException, ParseException {
-	    test.assertOutput("averagePrice", new File(TEST_PATH + "results/averagePrice.txt"));
-	}
+    private static final String TEST_PATH = "src/test/resources/";
+
+    private static PigTest test;
+
+    @BeforeClass
+    public static void setUp() throws IOException, ParseException {
+        test = new PigTest("src/main/resources/pig_with_2_outputs_short.pig");
+        test.override("products",
+                        "products = LOAD '"
+                                        + TEST_PATH
+                                        + "input/customer_records_map_reduce_input.json' USING JsonLoader('sessionId:int,customerCategoryId:int,products:{(id:int,name:chararray,category:chararray,bought:boolean,price:double)}');");
+        test.override("categories", "categories = LOAD '" + TEST_PATH
+                        + "input/customer_categories.db' AS (categoryId:int,age:chararray,gender:chararray);");
+    }
+
+    @Test
+    public void testTopFiveProducts() throws IOException, ParseException {
+        test.assertOutput("resultTopFiveProducts", new File(TEST_PATH + "results/resultTopFiveProducts.txt"));
+    }
+
+    @Test
+    public void testAverageSeenProducts() throws IOException, ParseException {
+        test.assertOutput("averageCountedProducts", new File(TEST_PATH + "results/averageSeenProducts.txt"));
+    }
+
+    @Test
+    public void testAverageBoughtProducts() throws IOException, ParseException {
+        test.assertOutput("resultAverageBoughtProducts", new File(TEST_PATH + "results/averageBoughtProducts.txt"));
+    }
+
+    @Test
+    public void testAverageSpent() throws IOException, ParseException {
+        test.assertOutput("averagePrice", new File(TEST_PATH + "results/averagePrice.txt"));
+    }
 
 }
