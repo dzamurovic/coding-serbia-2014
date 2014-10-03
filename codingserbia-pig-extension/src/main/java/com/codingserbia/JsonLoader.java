4f506be9dc259d20091796021b10a83667eb3f59
dzamurovic <ezamur@gmail.com>
2014-10-03 17:14:32 +0200

    Adding JSON schema.

--- codingserbia-pig-extension/src/main/java/com/codingserbia/JsonLoader.java
+++ codingserbia-pig-extension/src/main/java/com/codingserbia/JsonLoader.java
@@ -10,20 +10,25 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.pig.Expression;
 import org.apache.pig.LoadFunc;
+import org.apache.pig.LoadMetadata;
+import org.apache.pig.ResourceSchema;
+import org.apache.pig.ResourceStatistics;
 import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigSplit;
 import org.apache.pig.data.BagFactory;
 import org.apache.pig.data.DataBag;
+import org.apache.pig.data.DataType;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.data.TupleFactory;
+import org.apache.pig.impl.logicalLayer.schema.Schema;
+import org.apache.pig.impl.logicalLayer.schema.Schema.FieldSchema;
 
 import com.codingserbia.dto.CustomerSession;
 import com.codingserbia.dto.Product;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class JsonLoader extends LoadFunc {
-
-    private int counter = 0;
+public class JsonLoader extends LoadFunc implements LoadMetadata {
 
     private ObjectMapper jsonMapper;
 
@@ -80,6 +85,52 @@ public class JsonLoader extends LoadFunc {
     @Override
     public void setLocation(String location, Job job) throws IOException {
         FileInputFormat.setInputPaths(job, location);
+    }
+
+    @Override
+    public String[] getPartitionKeys(String arg0, Job arg1) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ResourceSchema getSchema(String location, Job job) throws IOException {
+        Schema schema = new Schema();
+
+        FieldSchema sessionId = new FieldSchema("sessionId", DataType.LONG);
+        schema.add(sessionId);
+
+        FieldSchema customerGroupId = new FieldSchema("customerCategoryId", DataType.LONG);
+        schema.add(customerGroupId);
+
+        FieldSchema customerGroupDescription = new FieldSchema("customerCategoryDescription", DataType.CHARARRAY);
+        schema.add(customerGroupDescription);
+
+        FieldSchema products = new FieldSchema("products", DataType.BAG);
+        FieldSchema arraySchema = new FieldSchema("ARRAY_ELEM", DataType.TUPLE);
+        arraySchema.schema = new Schema();
+        arraySchema.schema.add(new FieldSchema("productId", DataType.LONG));
+        arraySchema.schema.add(new FieldSchema("productName", DataType.CHARARRAY));
+        arraySchema.schema.add(new FieldSchema("productCategoryId", DataType.LONG));
+        arraySchema.schema.add(new FieldSchema("bought", DataType.INTEGER));
+        arraySchema.schema.add(new FieldSchema("price", DataType.DOUBLE));
+        products.schema = new Schema(arraySchema);
+
+        schema.add(products);
+
+        return new ResourceSchema(schema);
+    }
+
+    @Override
+    public ResourceStatistics getStatistics(String arg0, Job arg1) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setPartitionFilter(Expression arg0) throws IOException {
+        // TODO Auto-generated method stub
+
     }
 
 }
